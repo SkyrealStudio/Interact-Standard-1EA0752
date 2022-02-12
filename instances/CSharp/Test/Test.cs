a608953b9087e7b86ce76aa0242594e8b280dc97
TianC Wang <thewater2016@qq.com>
2022-02-12 16:51:48 +0800

    Add Test

---
+++ instances/CSharp/Test/Test.cs
@@ -0,0 +1,18 @@
+﻿namespace Skyreal.Universal.Tests
+{
+    using Static;
+    class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            Queue<TextScriptReader.TextScriptMethod> tx = TextScriptReader.GetMethods("C:\\Users\\thewa\\Documents\\Codes\\Interact-Standard-1EA0752\\instances\\CSharp\\Test\\test.txt");
+            while (tx.Count > 0)
+            {
+                TextScriptReader.TextScriptMethod method = tx.Dequeue();
+                Console.WriteLine("Method Name: " + method.MethodName);
+                foreach (string str in method.Parameters)
+                    Console.WriteLine("-: " + str);
+            }
+        }
+    }
+}
